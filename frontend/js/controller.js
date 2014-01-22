01a6fcc4cb49bb8b586091ff85114ee5cd15c8c7
xtofl <xtofl@server.fake>
2014-01-22 19:28:23 +0100

    renamed 'ingredient' to 'grocery'

--- frontend/js/controller.js
+++ frontend/js/controller.js
@@ -3,8 +3,8 @@ var growseryApp = angular.module('growceryApp', []);
 growseryApp.controller('GrowceryController', function($scope){
 	$scope.groceries = [
 	                    ];
-	$scope.addIngredient = function(){
-		$scope.groceries.push({name: "?", quantity: 1});
+	$scope.addGrocery = function(){
+		$scope.groceries.push({name: "", quantity: 1});
 	};
-	$scope.dishes = [{name: "dish1", ingredients: [{name: "selder"}]}];
+
 });
