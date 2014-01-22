bcdbf579239fefa08dbc946779ec717178fc9cd4
xtofl <xtofl@server.fake>
2014-01-22 19:23:48 +0100

    implemented: addIngredient

--- frontend/js/controller.js
+++ frontend/js/controller.js
@@ -2,9 +2,9 @@ var growseryApp = angular.module('growceryApp', []);
 
 growseryApp.controller('GrowceryController', function($scope){
 	$scope.groceries = [
-	                    {
-	                    	name: "selder",
-	                    	quantity: 1
-	                    }];
+	                    ];
+	$scope.addIngredient = function(){
+		$scope.groceries.push({name: "?", quantity: 1});
+	};
 	$scope.dishes = [{name: "dish1", ingredients: [{name: "selder"}]}];
 });
