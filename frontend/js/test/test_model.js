be67e133397f9a03833a24123bc313f718972b08
xtofl <xtofl@hotmail.com>
2014-02-16 14:01:39 +0100

    [refac] extract known setup variable

--- frontend/js/test/test_model.js
+++ frontend/js/test/test_model.js
@@ -31,30 +31,26 @@ require([ "model" ], function(model) {
 		equal(10, sum["a"], "all equal ingredients are summed");
 	});
 
-	function applyKnownSetup(scope){
-		
-		var additional = [
+	var setup = {
+		additional:[
 		                   {name: "a", quantity: 5},
-		                   {name: "a", quantity: 5}];
-		var dish1 = {
+		                   {name: "a", quantity: 5}],
+		dish1 : {
 				name: "dish1",
 				ingredients: [{name: "dish1.a", quantity: 1}, {name: "common.a", quantity: 1}],
 				targetQuantity: 5
-		};
-		var dish2 = {
+		},
+		dish2 : {
 				name: "dish2",
 				ingredients: [{name: "dish2.a", quantity: 1}, {name: "common.a", quantity: 1}],
 				targetQuantity: 5
-		};
-		scope.copyDishToMenu(dish1);
-		scope.copyDishToMenu(dish2);
-		additional.forEach(function(i){scope.addGrocery(i);});
-		
-		return {
-			dish1: dish1,
-			dish2: dish2,
-			additional: additional
-		};
+		}
+	};
+	
+	function applyKnownSetup(scope){
+		scope.copyDishToMenu(setup.dish1);
+		scope.copyDishToMenu(setup.dish2);
+		setup.additional.forEach(function(i){scope.addGrocery(i);});
 	};
 
 	test("Adding dishes to menu + additional groceries", function(){
@@ -74,7 +70,7 @@ require([ "model" ], function(model) {
 	test("removing dish from menu", function(){
 		var scope={};
 		model.create(scope);
-		var setup = applyKnownSetup(scope);
+		applyKnownSetup(scope);
 		
 		scope.removeDishFromMenu(setup.dish1);
 
@@ -91,7 +87,7 @@ require([ "model" ], function(model) {
 	test("removing additionall grocery", function(){
 		var scope={};
 		model.create(scope);
-		var setup = applyKnownSetup(scope);
+		applyKnownSetup(scope);
 		
 		scope.removeAdditionalGrocery(setup.additional[0]);
 
