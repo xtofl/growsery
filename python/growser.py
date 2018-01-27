0243494d537503763f0c7fad2b99b8650cc66fda
xtofl <kristoffel.pirard@gmail.com>
2018-01-27 11:09:11 +0100

    units should be singular

--- python/growser.py
+++ python/growser.py
@@ -12,10 +12,10 @@ basis_recipes = {
         Ingredient("melk", Amount(.8, "liter")),        
     ],
     "puree": [
-        Ingredient("patat", Amount(5*3, "stuks")),
+        Ingredient("patat", Amount(5*3, "stuk")),
         Ingredient("bakboter", Amount(1, "beetje")),
         Ingredient("nootmuskaat", Amount(1, "beetje")),
-        Ingredient("ei", Amount(1, "stuks")),
+        Ingredient("ei", Amount(1, "stuk")),
     ]
 }
 
@@ -25,27 +25,27 @@ recipes = {
     "groentensoep":
         Recipe(for_people=2, ingredients=[
             Ingredient("selder", Amount(.5, "stuk")),
-            Ingredient("wortel", Amount(4, "stuks")),
+            Ingredient("wortel", Amount(4, "stuk")),
             Ingredient("prei", Amount(2, "stelen")),
             Ingredient("ui", Amount(1, "stuk")),
             Ingredient("bouillon", Amount(1, "blokje"))
         ]),
     "papaschotel":
         Recipe(for_people=5, ingredients=[
-            Ingredient("witte kool", Amount(.5, "stuks")),
+            Ingredient("witte kool", Amount(.5, "stuk")),
             Ingredient("gehakt", Amount(500, "gram")),
             Ingredient("geraspte kaas", Amount(500, "gram")),
             ] + basis_recipes["puree"]),
     "salade met eitjes en ovenschotel met pasta en tomatensaus":
         Recipe(for_people=5, ingredients=[
             Ingredient("ijsbergsla", Amount(0.3, "krop")),
-            Ingredient("ei", Amount(6, "stuks")),
+            Ingredient("ei", Amount(6, "stuk")),
             Ingredient("krulletjes", Amount(500, "gram")),
             Ingredient("tomatenconcentraat", Amount(2, "blikje")),
         ] + basis_recipes["witte saus"]),
     "balletjes tomatensaus met boontjes":
         Recipe(for_people=5, ingredients=[
-            Ingredient("patatten", Amount(5*3, "stuks")),
+            Ingredient("patatten", Amount(5*3, "stuk")),
             Ingredient("gehakt", Amount(500, "gram")),
             Ingredient("boontjes", Amount(2, "pakjes")),
             ] + basis_recipes["witte saus"] + [
@@ -53,9 +53,9 @@ recipes = {
         ]),
     "pasta bolognese": Recipe(for_people=5, ingredients=[
         Ingredient("pasta", Amount(1.5, "pak")),
-        Ingredient("wortel", Amount(4, "stuks")),
+        Ingredient("wortel", Amount(4, "stuk")),
         Ingredient("gehakt", Amount(500, "gram")),
-        Ingredient("paprika", Amount(2, "stuks")),
+        Ingredient("paprika", Amount(2, "stuk")),
         Ingredient("tomatenconcentraat", Amount(2, "blikje")),
         Ingredient("emmental", Amount(300, "gram")),
         Ingredient("ui", Amount(1, "stuk")),
@@ -65,7 +65,7 @@ recipes = {
         Ingredient("kipfilet", Amount(500, "gram")),
         Ingredient("perziken in blik", Amount(1, "blikje")),
         Ingredient("currysaus", Amount(2, "zakjes")),
-        Ingredient("patatten", Amount(5*2, "stuks")),
+        Ingredient("patatten", Amount(5*2, "stuk")),
         Ingredient("melk", Amount(1, "liter"))
     ]),
 }
@@ -91,15 +91,15 @@ menu = [
 pantry = [
     Ingredient("bouillon", Amount(8, "blokje")),
 
-    Ingredient("witte kool", Amount(1, "stuks")),
+    Ingredient("witte kool", Amount(1, "stuk")),
     Ingredient("bakboter", Amount(2, "fles")),
     Ingredient("pasta", Amount(1, "pak")),
     Ingredient("krulletjes", Amount(500, "gram")),
     Ingredient("nootmuskaat", Amount(100, "beetje")),
     Ingredient("gehakt", Amount(500, "gram")),
-    Ingredient("ei", Amount(11, "stuks")),
+    Ingredient("ei", Amount(11, "stuk")),
     Ingredient("bouillon", Amount(8, "blojke")),
-    Ingredient("patat", Amount(20, "stuks")),
+    Ingredient("patat", Amount(20, "stuk")),
     Ingredient("perziken in blik", Amount(3, "blikje")),
     Ingredient("tomatenconcentraat", Amount(2, "blikje")),
 
@@ -109,7 +109,7 @@ pantry = [
 
     Ingredient("gehakt", Amount(300, "gram")),
     Ingredient("kipfilet", Amount(400, "gram")),
-    Ingredient("chipolata", Amount(10, "stuks")),
+    Ingredient("chipolata", Amount(10, "stuk")),
 
     Ingredient("kroketten", Amount(1, "zak")),
 ]
