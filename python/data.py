52fb552987f8aaffb911cb8b3a12bfeefa610c88
xtofl <kristoffel.pirard@gmail.com>
2018-02-03 10:37:24 +0100

    menu for next week

--- python/data.py
+++ python/data.py
@@ -26,7 +26,7 @@ recipes = {
     "beleg": Recipe(for_people=1, ingredients=[
         Ingredient("kaas", Amount(1, "plakje")),
         Ingredient("choco", Amount(0.05, "pot")),
-        Ingredient("papaboter", Amount(0.05, "bakje"))
+        Ingredient("papaboter", Amount(0.02, "bakje"))
     ]),
 
     "groentensoep":
@@ -61,7 +61,7 @@ recipes = {
     "pasta bolognese": Recipe(for_people=5, ingredients=[
         Ingredient("pasta", Amount(1.5, "pak")),
         Ingredient("wortel", Amount(4, "stuk")),
-        Ingredient("gehakt", Amount(500, "gram")),
+        Ingredient("gehakt", Amount(300, "gram")),
         Ingredient("paprika", Amount(2, "stuk")),
         Ingredient("tomatenconcentraat", Amount(2, "blikje")),
         Ingredient("emmental", Amount(300, "gram")),
@@ -75,66 +75,155 @@ recipes = {
         Ingredient("patatten", Amount(5*2, "stuk")),
         Ingredient("melk", Amount(1, "liter"))
     ]),
+    "cordon bleu": Recipe(for_people=5, ingredients=[
+        Ingredient("cordon bleu", Amount(5, "stuk"))
+    ]),
+    "wortelpuree": Recipe(for_people=5, ingredients=[
+        Ingredient("patatten", Amount(5*3, "stuk")),
+        Ingredient("wortel", Amount(5*2, "stuk")),
+        Ingredient("ei", Amount(1, "stuk")),
+        Ingredient("melk", Amount(0.5, "liter")),
+        Ingredient("nootmuskaat", Amount(1, "beetje"))
+    ]),
+    "aardappels in witte saus": Recipe(for_people=5, ingredients=[
+        Ingredient("patatten", Amount(5*2, "stuk")),
+        Ingredient("bloem", Amount(100, "gram")),
+        Ingredient("bakboter", Amount(3, "beetje")),
+        Ingredient("geraspte kaas", Amount(1, "zakje"))
+    ]),
+    "erwtjes en worteltjes": Recipe(for_people=5, ingredients=[
+        Ingredient("diepvrieserwten", Amount(0.5, "kg")),
+        Ingredient("wortel", Amount(8, "stuk"))
+    ]),
+    "witte kool": Recipe(for_people=5, ingredients=[
+        Ingredient("witte kool", Amount(0.5, "stuk")),
+    ]),
+    "kotelet": Recipe(for_people=5, ingredients=[
+        Ingredient("kotelet", Amount(5, "stuk")),
+        Ingredient("bakboter", Amount(1, "beetje"))
+    ]),
+    "kaassaus": Recipe(for_people=5, ingredients=[
+        Ingredient("bloem", Amount(100, "gram")),
+        Ingredient("bakboter", Amount(3, "beetje")),
+        Ingredient("geraspte kaas", Amount(1, "zakje")),
+        Ingredient("melk", Amount(1, "liter"))
+    ]),
+    "quiche met zalm en broccoli": Recipe(for_people=5, ingredients=[
+        Ingredient("kruimeldeeg", Amount(1, "stuk")),
+        Ingredient("ei", Amount(4, "stuk")),
+        Ingredient("melk", Amount(.25, "liter")),
+        Ingredient("diepvries broccoli", Amount(.5, "zak")),
+        Ingredient("geraspte kaas", Amount(1, "zakje")),
+    ]),
+    "risotto": Recipe(for_people=5, ingredients=[
+        Ingredient("ui", Amount(1, "stuk")),
+        Ingredient("bakboter", Amount(1, "beetje")),
+        Ingredient("risotto", Amount(200, "gram"))
+    ]),
+    "macaroni met ham, kaas en broccoli": Recipe(for_people=5, ingredients=[
+        Ingredient("krulletjes", Amount(300, "gram")),
+        Ingredient("hamblokjes", Amount(150*5, "gram")),
+        #kaassaus
+        Ingredient("bloem", Amount(100, "gram")),
+        Ingredient("bakboter", Amount(3, "beetje")),
+        Ingredient("geraspte kaas", Amount(1, "zakje")),
+        Ingredient("melk", Amount(1, "liter")),
+        Ingredient("ketchup", Amount(0.3, "fles")),
+    ]),
+    "biefstuk": Recipe(for_people=5, ingredients=[
+        Ingredient("biefstuk", Amount(4, "stuk")),
+        Ingredient("bakboter", Amount(1, "beetje")),
+    ]),
+    "senseo": Recipe(for_people=2, ingredients=[Ingredient("senseo", Amount(2, "pad"))]),
+    "nespresso": Recipe(for_people=1, ingredients=[Ingredient("nespresso", Amount(1, "capsule"))])
 }
 
 
 menu = [
     Serving(recipe_name="groentensoep", for_people=5),
-    Serving(recipe_name="papaschotel", for_people=5),
-    
-    Serving(
-        for_people=7,
-        recipe_name="salade met eitjes en ovenschotel met pasta en tomatensaus",
-        ),
-    
-    Serving(recipe_name="balletjes tomatensaus met boontjes", for_people=7),
-    
+
+    Serving(recipe_name="cordon bleu", for_people=5),
+    Serving(recipe_name="wortelpuree", for_people=5),
+
+    Serving(recipe_name="aardappels in witte saus", for_people=5),
+    Serving(recipe_name="erwtjes en worteltjes", for_people=5),
+    Serving("biefstuk", for_people=5),
+
+    Serving(recipe_name="witte kool", for_people=5),
+    Serving("kaassaus", 5),
+    Serving(recipe_name="kotelet", for_people=5),
+
     Serving(recipe_name="pasta bolognese", for_people=5),
-    
-    Serving(recipe_name="kip met currysaus, perziken en patatten", for_people=5),
+
+    Serving("quiche met zalm en broccoli", for_people=5),
+    Serving("risotto", for_people=5),
+
+    Serving("macaroni met ham, kaas en broccoli", for_people=5),
 
 ] + [
     Serving(recipe_name="koekjes", for_people=3),
     Serving(recipe_name="fruit", for_people=5),
-    Serving(recipe_name="beleg", for_people=5)] * 7
+    Serving(recipe_name="beleg", for_people=5),
+    Serving(recipe_name="senseo", for_people=2),
+    Serving(recipe_name="nespresso", for_people=2)
+    ] * 7
 
 
 pantry = [
     Ingredient("bouillon", Amount(8, "blokje")),
+    Ingredient("citroenthee", Amount(0, "doosje")),
+    Ingredient("senseo", Amount(10, "pad")),
+    Ingredient("nespresso", Amount(40, "capsule")),
 
-    Ingredient("witte kool", Amount(1, "stuk")),
-    Ingredient("bakboter", Amount(2, "fles")),
-    Ingredient("pasta", Amount(1, "pak")),
-    Ingredient("krulletjes", Amount(500, "gram")),
+    Ingredient("patat", Amount(20, "stuk")),
+
+    Ingredient("currysaus", Amount(1, "zakje")),
+
     Ingredient("nootmuskaat", Amount(100, "beetje")),
-    Ingredient("gehakt", Amount(500, "gram")),
-    Ingredient("ei", Amount(11, "stuk")),
-    Ingredient("bouillon", Amount(8, "blojke")),
-    Ingredient("patat", Amount(20+30, "stuk")),
-    Ingredient("perziken in blik", Amount(3, "blikje")),
-    Ingredient("tomatenconcentraat", Amount(2, "blikje")),
 
-    Ingredient("currysaus", Amount(3, "zakjes")),
-    Ingredient("choco", Amount(1.5, "pot")),
+    #frigo
+    Ingredient("witte kool", Amount(.5, "stuk")),
+    Ingredient("bakboter", Amount(.1, "fles")),
+    Ingredient("ei", Amount(0, "stuk")),
+    Ingredient("ketchup", Amount(0, "fles")),
+
+    Ingredient("risotto", Amount(1.5, "kg")),
+    Ingredient("basmati", Amount(1.2, "kg")),
+
+    Ingredient("bloem", Amount(3, "kg")),
+    Ingredient("pasta", Amount(3, "pak")),
+    Ingredient("krulletjes", Amount(500, "gram")),
+
+    Ingredient("perziken in blik", Amount(1, "blikje")),
+    Ingredient("tomatenconcentraat", Amount(3, "blikje")),
+    Ingredient("yoghurtjes", Amount(6, "potje")),
+
+    Ingredient("choco", Amount(1, "pot")),
 
     Ingredient("ijsbergsla", Amount(1, "krop")),
 
-    Ingredient("ui", Amount(1, "stuk")),
+    Ingredient("ui", Amount(0, "stuk")),
 
     Ingredient("gehakt", Amount(300, "gram")),
-    Ingredient("kipfilet", Amount(400, "gram")),
-    Ingredient("chipolata", Amount(10, "stuk")),
+    Ingredient("kipfilet", Amount(0, "gram")),
+    Ingredient("chipolata", Amount(0, "stuk")),
+    Ingredient("kippenbout", Amount(4, "stuk")),
+    Ingredient("kalkoenschnitzel", Amount(4, "stuk")),
 
     Ingredient("kroketten", Amount(1, "zak")),
 
-    Ingredient("fruit", Amount(10, "stuk")),
+    Ingredient("diepvries broccoli", Amount(1, "zak")),
+    Ingredient("diepvrieserwten", Amount(1, "kg")),
+
+    Ingredient("fruit", Amount(2, "stuk")),
     Ingredient("koekjes", Amount(20, "stuk")),
 ]
 
 extras = [
-    Ingredient("afwasblokjes", Amount(1, "doos"))
+    Ingredient("afwasblokjes", Amount(1, "doos")),
 ]
 
 conversions = {
-    ("beetje", "fles"): lambda x: Amount(x.number / 100., "fles")
+    ("beetje", "fles"): lambda x: Amount(x.number / 100., "fles"),
+    ("gram", "kg"): lambda x: Amount(x.number / 1000, "kg"),
 }
