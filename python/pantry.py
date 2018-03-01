7a8553ac497c864323fd3df41052c58923041a6f
xtofl <kristoffel.pirard@gmail.com>
2018-03-01 21:24:05 +0100

    [refac] extract data into separate files

---
+++ python/pantry.py
@@ -0,0 +1,57 @@
+from entities import *
+from units import *
+from recipes import *
+
+pantry = [
+    Ingredient("bouillon", Amount(8, stuk)),
+    Ingredient("citroenthee", Amount(1, doosje)),
+    Ingredient("senseo", Amount(30, senseo_pad)),
+    Ingredient("nespresso", Amount(0, koffie_capsule)),
+
+    Ingredient("patat", Amount(20, stuk)),
+
+    Ingredient("currysaus", Amount(1, zakje)),
+
+    Ingredient("nootmuskaat", Amount(50, beetje)),
+
+    #frigo
+    Ingredient("witte kool", Amount(0, stuk)),
+    Ingredient("bakboter", Amount(1, fles)),
+    Ingredient("ei", Amount(9, stuk)),
+    Ingredient("ketchup", Amount(1, fles)),
+
+    Ingredient("risotto", Amount(1.5, kg)),
+    Ingredient("basmati", Amount(1.2, kg)),
+
+    Ingredient("bloem", Amount(3, kg)),
+    Ingredient("pasta", Amount(0, pak)),
+    Ingredient("krulletjes", Amount(1000, gram)),
+
+    Ingredient("perziken in blik", Amount(1, blik)),
+    Ingredient("tomatenconcentraat", Amount(0, blik)),
+    Ingredient("yoghurtjes", Amount(0, potje)),
+
+    Ingredient("choco", Amount(.5, pot)),
+
+    Ingredient("ijsbergsla", Amount(0, stuk)),
+
+    Ingredient("ui", Amount(20, stuk)),
+
+    Ingredient("gehakt", Amount(0, gram)),
+    Ingredient("kipfilet", Amount(0, gram)),
+    Ingredient("chipolata", Amount(0, stuk)),
+    Ingredient("kippenbout", Amount(3, stuk)),
+    Ingredient("kalkoenschnitzel", Amount(4, stuk)),
+    Ingredient("kotelet", Amount(4, stuk)),
+
+    Ingredient("kroketten", Amount(0, zak)),
+
+    Ingredient("diepvries broccoli", Amount(0, zak)),
+    Ingredient("diepvrieserwten", Amount(0, kg)),
+
+    Ingredient("fruit", Amount(2, stuk)),
+    Ingredient("koekjes", Amount(12, stuk)),
+    Ingredient("granola", Amount(0.1, zak)),
+
+    Ingredient("melk", Amount(1, liter))
+]
