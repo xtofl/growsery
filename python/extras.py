161276d2e75f067d55867850b90585faa77eac7b
xtofl <kristoffel.pirard@gmail.com>
2018-03-24 14:42:43 +0100

    meer extra

--- python/extras.py
+++ python/extras.py
@@ -7,4 +7,6 @@ extras = [
     Ingredient("cursusbladen geruit", Amount(2, pak)),
     Ingredient("gele marker", Amount(1, stuk)),
     Ingredient("tippex", Amount(1, stuk)),
+    Ingredient("batterijtjes", Amount(3, stuk)),
+    Ingredient("frigolampje", Amount(2, stuk))
 ]
