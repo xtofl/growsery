c637de9e49051479284b8b248ff84f38405c8a1f
xtofl <kristoffel.pirard@gmail.com>
2019-03-16 15:02:37 +0100

    update lijst.txt when run

--- python/data/shopping_list.sh
+++ python/data/shopping_list.sh
@@ -2,4 +2,5 @@
 
 here=$(cd $(dirname $0) && pwd)
 cd $here
-PYTHONPATH=../src python3 -m shopping_list --pantry pantry.txt --shop shop.txt
+PYTHONPATH=../src python3 -m shopping_list --pantry pantry.txt --shop shop.txt > lijst.txt
+cat lijst.txt
