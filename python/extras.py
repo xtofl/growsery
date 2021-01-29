cc387f10aea49ff1ff97b92556d1e827bf423018
Kristoffel Pirard <kristoffel.pirard@midiagnostics.com>
2021-01-29 14:12:06 +0100

    update pantry/extras

--- python/extras.py
+++ python/extras.py
@@ -2,11 +2,9 @@ from entities import Ingredient, Amount
 from units import *
 
 extras = [
-    Ingredient("tandenborstel-opzetstuk", Amount(1, pak)),
-    Ingredient("tandenborstel voor blokjes", Amount(1, stuk)),
-    Ingredient("cursusbladen geruit", Amount(2, pak)),
-    Ingredient("gele marker", Amount(1, stuk)),
-    Ingredient("tippex", Amount(1, stuk)),
-    Ingredient("batterijtjes", Amount(3, stuk)),
-    Ingredient("frigolampje", Amount(2, stuk))
+    Ingredient("mocha-thee", Amount(1, doosje)),
+    Ingredient("royco kippensoep", Amount(1, doosje)),
+    Ingredient("shampoo", Amount(1, fles)),
+    Ingredient("amandelmelk", Amount(2, fles)),
+    Ingredient("gemberthee", Amount(1, doosje))
 ]
--- python/pantry.py
+++ python/pantry.py
@@ -4,78 +4,93 @@ from recipes import *
 
 pantry = [
     Ingredient("citroenthee", Amount(0, doosje)),
-    Ingredient("senseo", Amount(36, senseo_pad)),
-    Ingredient("nespresso", Amount(6, koffie_capsule)),
+    Ingredient("prei", Amount(0, stuk)),
+    Ingredient("chocola", Amount(5*200, gram)),
 
-    Ingredient("bouillon", Amount(14*4, stuk)),
-    Ingredient("groentenbouillon", Amount(34, stuk)),
+    Ingredient("bouillon", Amount(1, stuk)),
+    Ingredient("groentenbouillon", Amount(8, stuk)),
 
-    Ingredient("patat", Amount(0, stuk)),
+    Ingredient("patat", Amount(20, stuk)),
 
-    Ingredient("nootmuskaat", Amount(5, beetje)),
+    Ingredient("nootmuskaat", Amount(1, potje)),
 
+    Ingredient("selder", Amount(0, stuk)),
+    Ingredient("puddingpoeder", Amount(3, zakje)),
     Ingredient("currysaus", Amount(3, zakje)),
     Ingredient("kinnekessuiker", Amount(1.8, kg)),
     Ingredient("bloemsuiker", Amount(200, gram)),
 
     #frigo
-    Ingredient("witte kool", Amount(.0, stuk)),
+    Ingredient("witte kool", Amount(0, stuk)),
     Ingredient("bakboter", Amount(2, fles)),
-    Ingredient("ei", Amount(1, stuk)),
+    Ingredient("ei", Amount(3, stuk)),
 
-    Ingredient("leerdammer", Amount(10, plakje)),
-    Ingredient("wortel", Amount(0, stuk)),
-    Ingredient("witloof", Amount(0, stuk)),
+    Ingredient("leerdammer", Amount(0, plakje)),
+    Ingredient("wortel", Amount(28, stuk)),
+    Ingredient("witloof", Amount(4, stuk)),
     Ingredient("ijsbergsla", Amount(0, stuk)),
-    Ingredient("hamblokjes", Amount(2, doosje)),
+    Ingredient("hamblokjes", Amount(0, doosje)),
+    Ingredient("salami", Amount(1, pak)),
 
     #kast
-    Ingredient("risotto", Amount(1, kg)),
+    Ingredient("vanillesuiker", Amount(4, zakje)),
+    Ingredient("risotto", Amount(.5, kg)),
     Ingredient("basmati", Amount(.8, kg)),
 
-    Ingredient("bloem", Amount(3, kg)),
+    Ingredient("spaghettikruiden", Amount(2, potje)),
+    Ingredient("kaneel", Amount(1, potje)),
+    Ingredient("honing", Amount(1, pot)),
+
+    Ingredient("bloem", Amount(1, kg)),
     Ingredient("pasta", Amount(700,gram)),
     Ingredient("krulletjes", Amount(500, gram)),
+    Ingredient("schelpjes", Amount(0, gram)),
 
-    Ingredient("fruit", Amount(2, stuk)),
+    Ingredient("fruit", Amount(15, stuk)),
     Ingredient("banaan", Amount(0, stuk)),
     Ingredient("appel", Amount(5, stuk)),
     Ingredient("druiven", Amount(0, doos)),
-    Ingredient("persappelsienen", Amount(4, stuk)),
+    Ingredient("persappelsienen", Amount(15, stuk)),
 
-    Ingredient("perziken in blik", Amount(0, blik)),
-    Ingredient("tomatenconcentraat", Amount(4, blik)),
+    Ingredient("perziken in blik", Amount(1, blik)),
+    Ingredient("tomatenconcentraat", Amount(1, blik)),
     Ingredient("yoghurtjes", Amount(0, potje)),
 
-    Ingredient("choco", Amount(2, pot)),
+    Ingredient("kwatta", Amount(1, pot)),
+    Ingredient("nutella", Amount(1, pot)),
 
-    Ingredient("ui", Amount(10, stuk)),
+    Ingredient("ui", Amount(2, stuk)),
+    Ingredient("look", Amount(10, teentje)),
+
+    Ingredient("olijfolie", Amount(1, liter)),
 
     # diepvries
     Ingredient("kroketten", Amount(2.5, zak)),
 
     Ingredient("diepvries broccoli", Amount(1.3, zak)),
-    Ingredient("diepvrieserwten", Amount(.7, kg)),
+    Ingredient("diepvrieserwten", Amount(.5, kg)),
 
-    Ingredient("gehakt", Amount(0, gram)),
+    Ingredient("gehakt", Amount(500, gram)),
     Ingredient("kipfilet", Amount(0, gram)),
     Ingredient("chipolata", Amount(4, stuk)),
-    Ingredient("kalkoenschnitzel", Amount(4, stuk)),
+    Ingredient("kalkoenschnitzel", Amount(0, stuk)),
     Ingredient("kotelet", Amount(0, stuk)),
     Ingredient("biefstuk", Amount(0, stuk)),
     Ingredient("kippenbout", Amount(0, stuk)),
+    Ingredient("notenburger", Amount(0, stuk)),
 
-    Ingredient("koekjes", Amount(20, stuk)),
-    Ingredient("granola", Amount(0.5, zak)),
+    Ingredient("koekjes", Amount(0, stuk)),
+    Ingredient("granola", Amount(0, zak)),
 
     Ingredient("melk", Amount(8, liter)),
+    Ingredient("papaboter", Amount(1.3, bakje)),
 
     Ingredient("tomaten passata", Amount(2, doos)),
     Ingredient("mais", Amount(3, blik)),
-    Ingredient("zwanworstjes", Amount(2, blik)),
+    Ingredient("zwanworstjes", Amount(0, blik)),
     Ingredient("appelmoes", Amount(1, pot)),
     Ingredient("rode kool", Amount(2, pot)),
-    Ingredient("wraps", Amount(0, stuk)),
+    Ingredient("wraps", Amount(6, stuk)),
     Ingredient("confituur", Amount(1, pot)),
 
     Ingredient("mayo", Amount(0, fles)),
